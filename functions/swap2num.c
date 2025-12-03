884967b03057c108fc17ca30f3c5330c42e37210
ashishbtech <ashishbtech.cs@gmail.com>
2025-12-03 21:16:24 +0530

    I love you 3000

---
+++ functions/swap2num.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+    int a;
+    printf("Enter a :");
+    scanf("%d", &a);
+
+    int b;
+    printf("Enter b :");
+    scanf("%d", &b);
+
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
+    printf("After SWAPPING :\n");
+    printf("The value of a is %d \n", a);
+    printf("The value of b is %d \n", b);
+
+    return 0;
+}
