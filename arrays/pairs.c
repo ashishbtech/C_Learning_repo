390819dee74fca7859ddf23e6184f823d364cea5
ashishbtech <ashishbtech.cs@gmail.com>
2025-12-24 21:07:11 +0530

    Christmas is tommorow

---
+++ arrays/pairs.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+    int arr[8] = {1, 2, 3, 4, 5, 6, 7, 8};
+    int totalpairs = 0;
+    int x = 12;
+    for (int i = 0; i < 8; i++)
+    {
+        for (int j = i+1; j < 8; j++)
+        {
+            if (arr[i] + arr[j] == x)
+            {
+                totalpairs++;
+
+                printf("(%d,%d)", arr[i], arr[j]);
+            }
+        }
+    }
+    printf("\nTotal pairs are %d", totalpairs);
+    return 0;
+}
